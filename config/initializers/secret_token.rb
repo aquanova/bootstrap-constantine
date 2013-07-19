39a3e9a420d79112a9f5c54449abf48f1159adcf
Constantine Zaytsev <bassneck@gmail.com>
2013-07-19 14:52:21 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Verso::Application.config.secret_key_base = '4708e8710becbfbc52748b05d50bd167b549a274789d6c8e4f02793b65bedab73c2bdbeec12fd68ba2bb99036a258fda2c38cf0fa2db3f93fa3b777427041ba9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Verso::Application.config.session_store :cookie_store, key: '_verso_session'
