0c2759c0e4cfe1fb5a82f532668406f2c6a612d1
Constantine Zaytsev <bassneck@gmail.com>
2013-07-23 17:20:19 +0400

    Opted out from using rails 4 for now

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Verso::Application.config.secret_key_base = '4708e8710becbfbc52748b05d50bd167b549a274789d6c8e4f02793b65bedab73c2bdbeec12fd68ba2bb99036a258fda2c38cf0fa2db3f93fa3b777427041ba9'
+Verso::Application.config.secret_token = 'e2fc421887926b9c5adabef09e54334a41215f7fd287ad7499db9d820341056bab2813d365c9bb7a99d708b3b9f69dfbce80cab579bdde1258a4c0b9967336f4'
