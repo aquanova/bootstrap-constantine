96db6f1d908e89e1abbab33116ecfb0e7a111db2
Constantine Zaytsev <bassneck@gmail.com>
2013-07-26 15:46:29 +0400

    Fixed the registrations controller layout proc

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module Verso
       DeviseController.respond_to :html, :json
 
       Devise::SessionsController.layout 'admin_unregistered'
-      Devise::RegistrationsController.layout Proc.new { |c| c.action_name == 'new' || 'create' ? 'admin_unregistered' : 'admin' }
+      Devise::RegistrationsController.layout Proc.new { |c| %w(new create).include? c.action_name ? 'admin_unregistered' : 'admin' }
       Devise::ConfirmationsController.layout "admin"
       Devise::UnlocksController.layout "admin"
       Devise::PasswordsController.layout "admin"
